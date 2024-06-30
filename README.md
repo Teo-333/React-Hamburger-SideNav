dc54a1f2b2b86c22c608c76db2830bee65074a32
Teo-333 <fedrushkevich@gmail.com>
2024-06-30 13:04:55 +0300

    (RHCV-m): Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Getting Started with Create React App
+# React Hamburger Side Nav
+Inspiration taken from : (https://daily-dev-tips.com/posts/animated-hamburger-side-menu/#html-structure-for-side-menu)
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const mainNav = document.getElementById("main-nav");
+
+    const handleClick = (event) => {
+      if (!event.target.matches(".nav-menu, .nav-menu *")) return;
+      mainNav.classList.toggle("nav-open");
+    };
+
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <nav id="main-nav" className="nav">
+        <a href="/" className="logo">
+          <img
+            width="50"
+            src="https://daily-dev-tips.com/images/logo.png"
+            alt="logo"
+          />
         </a>
-      </header>
+        <div className="nav-menu">
+          <span className="nav-menu-item">Menu</span>
+          <span className="nav-menu-cross"></span>
+        </div>
+        <div className="nav-menu-side">
+          <nav className="nav-menu-side-nav">
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">Work</a>
+              </li>
+              <li>
+                <a href="/">Blog</a>
+              </li>
+              <li>
+                <a href="/">Something Else</a>
+              </li>
+              <li>
+                <a href="/">Contact Me</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </nav>
     </div>
   );
 }
